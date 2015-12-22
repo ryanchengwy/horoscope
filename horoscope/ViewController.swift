a66dc71d4e4103e1fe51c36ffc4e623a9f5c62ec
RyanC <ryan.chengwy@gmail.com>
2015-12-22 17:56:11 +0800

    First upload

--- horoscope/ViewController.swift
+++ horoscope/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var showHoroscope: UIButton!
+    @IBOutlet weak var showMyHoroscope: UIButton!
+    var arrayHoroscope = ["水瓶座", "雙魚座", "牡羊座", "金牛座", "雙子座", "巨蟹座", "獅子座", "處女座", "天秤座", "天蝎座", "射手座", "山羊座"]
 
+    var arrayPosition = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showHoroscope.setTitle(arrayHoroscope[arrayPosition], forState: . Normal)
+        showMyHoroscope.setTitle("跳回我的星座", forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showHoroscopePressed(sender: AnyObject) {
+        if arrayPosition <= 10 {
+            showHoroscope.setTitle(arrayHoroscope[arrayPosition + 1], forState: .Normal)
+            arrayPosition += 1
+        } else {
+            arrayPosition = 0
+            showHoroscope.setTitle(arrayHoroscope[arrayPosition], forState: .Normal)
+            arrayPosition += 1
+        }
+    }
+    @IBAction func showMyHoroscopePressed(sender: AnyObject) {
+        arrayPosition = 7
+        showHoroscope.setTitle(arrayHoroscope[arrayPosition], forState: .Normal)
+    }
+    
 
 
 }
